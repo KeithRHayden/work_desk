c86aa64e9250f7ad93767027d8336d9ba83181a7
Keith R Hayden <keith.hydn345@gmail.com>
2026-07-16 12:58:38 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'work-desk-v1';
+const ASSETS = [
+  '/work_desk/',
+  '/work_desk/index.html',
+  '/work_desk/icon-192.png',
+  '/work_desk/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const net = fetch(e.request).then(res => {
+        if (res && res.status === 200) {
+          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        }
+        return res;
+      }).catch(() => null);
+      return cached || net;
+    })
+  );
+});
